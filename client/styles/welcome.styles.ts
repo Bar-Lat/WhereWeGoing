cacbeb2400b39e254a786fdd4d1718d6cfb845de
Bar-Lat <latusek.bartosz@gmail.com>
2026-04-18 23:58:58 +0200

    modified:   client/app/(auth)/welcome.tsx 	modified:   client/styles/welcome.styles.ts

--- client/styles/welcome.styles.ts
+++ client/styles/welcome.styles.ts
@@ -6,7 +6,6 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
-    paddingTop: 90,
     paddingBottom: 40,
   },
   header: {
@@ -15,8 +14,10 @@ export const styles = StyleSheet.create({
     zIndex: 20,
   },
   logo: {
-    width: 260,
+    width: '75%', // Skaluje się z ekranem
+    maxWidth: 280, // Ale nie będzie większe niż 260px na iPadzie
     height: 60,
+    marginTop: '15%',
   },
   middleContent: {
     flex: 1,
@@ -29,6 +30,7 @@ export const styles = StyleSheet.create({
     width: width, // Cała szerokość ekranu dla poprawnego działania `pagingEnabled`
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 24, // Odsuwamy zawartość od krawędzi ekranu
   },
   slideContainer: {
     width: width - 48, // Karta węższa niż ekran o 24px z każdej strony
@@ -50,18 +52,23 @@ export const styles = StyleSheet.create({
   // ------------------
   slideTextWrapper: {
     zIndex: 10,
-    marginBottom: 20,
   },
   slideTitle: {
     color: '#ffffff',
     fontSize: 32,
     fontWeight: 'bold',
     marginBottom: 8,
+    textShadowColor: 'rgba(0, 0, 0, 0.8)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 12
   },
   slideDescription: {
     color: '#f0f0f0',
     fontSize: 16,
     lineHeight: 24,
+    textShadowColor: 'rgba(0, 0, 0, 0.8)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 12
   },
   dotsContainer: {
     flexDirection: 'row',
@@ -69,36 +76,28 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
     gap: 8,
+    marginBottom: 10,
   },
   dot: {
     width: 8,
     height: 8,
     borderRadius: 4,
   },
+  
+  // --- Stopka ---
   footer: {
     width: '100%',
     paddingHorizontal: 24,
     gap: 8,
-    marginTop: 20,
+    marginTop: 10,
     zIndex: 20,
-    marginBottom: 20
-  },
-  secondaryButton: {
-    paddingVertical: 12,
-    borderRadius: 16,
-    alignItems: 'center',
-    width: '80%', 
-    alignSelf: 'center',
-  },
-  secondaryButtonText: {
-    fontSize: 14,
-    fontWeight: '600',
+    marginBottom: 10,
   },
   footerBase: {
-    marginTop: 20,
+    marginTop: 10,
     alignItems: 'center',
   },
   loginPrompt: {
-    padding: 0,
+    padding: 10, // Trochę większy klikalny obszar dla wygody
   },
 });
