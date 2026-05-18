dc5bd8a19c1b105f23009fd69c16e540ad1b7201
otu1231 <otu5795123@gmail.com>
2026-05-18 17:56:52 +0200

    feat(trip): add trip days repository and integrate with trip generation

---
+++ server/repositories/tripDays.repository.js
@@ -0,0 +1,27 @@
+const { supabaseDbClient } = require('../configs/supabaseClient');
+
+const TABLE = 'trip_days';
+
+const createTripDays = async (days) => {
+  const { data, error } = await supabaseDbClient
+    .from(TABLE)
+    .insert(days)
+    .select('*');
+
+  return { data, error };
+};
+
+const getTripDaysByTripId = async (tripId) => {
+  const { data, error } = await supabaseDbClient
+    .from(TABLE)
+    .select('*')
+    .eq('trip_id', tripId)
+    .order('day_number', { ascending: true });
+
+  return { data, error };
+};
+
+module.exports = {
+  createTripDays,
+  getTripDaysByTripId,
+};
